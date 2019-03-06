e4f5cb9d3a41f57bd2c144727ca778ab3023f03d
unknown <devsrawt@gmail.com>
2019-03-06 20:55:00 +0530

    before deployment

---
+++ README.md
@@ -0,0 +1,10 @@
+# 15-puzzleGame
+15 puzzle game made with MERN stack
+clone repo then install all dependencies by :
+  1. cd server npm i 
+  2. cd client npm i
+
+then :
+  > cd server
+  > npm run dev
+ 
--- server/models/index.js
+++ server/models/index.js
@@ -6,6 +6,21 @@ mongoose.connect("mongodb://localhost/puzzle", {
   keepAlive: true,
   useNewUrlParser: true
 });
+// (async function connectDB() {
+//   try {
+//     console.log(db);
+//     await mongoose
+//       .connect(db, {
+//         keepAlive: true,
+//         useNewUrlParser: true
+//       })
+//       .then(() => {
+//         console.log("DB CONNECTED SUCCESSFULLY");
+//       });
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// })();
 
 module.exports.Player = require("./players");
 module.exports.Game = require("./game");
